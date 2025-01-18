41d6945cb3340ea3390bfd8da6cf1ed2c56209c8
User <shurbe96@mail.ru>
2025-01-19 00:06:45 +0700

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,19 @@
 from typing import List, Dict
 
 
-def filter_by_state(list_dict: list[dict], value_key: str = "EXECUTED") -> list[dict]:
+def filter_by_state(list_dict: list[Dict], value_key: str = "EXECUTED") -> list[Dict]:
     """Функуия, которая возвращает новый список словарей по указанному ключу"""
     new_list_dict = []
     for every_dict in list_dict:
-       if every_dict["stste"] == value_key:
+       if every_dict["state"] == value_key:
            new_list_dict.append(every_dict)
     return new_list_dict
+
+
+def sort_by_date(list_dict: list[Dict], arg_for_sort: bool = True) -> list[Dict]:
+    """Функция сортировки списка банковских операций по дате (по умолчанию-убывание)"""
+    sort_list = sorted(list_dict, key=lambda every_dict: every_dict.get("date", ""), reverse=arg_for_sort)
+    return sort_list
 
 
 if __name__ == "__main__":
